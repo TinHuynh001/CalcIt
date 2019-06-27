ed61e1d3e2efa4db6c5321cac56ed90f609f67a2
TinHuynh001 <Tin.Huynh001@umb.edu>
2019-06-26 20:32:14 -0500

    parse number input

--- app/src/main/java/com/calcit/MainActivity.java
+++ app/src/main/java/com/calcit/MainActivity.java
@@ -3,6 +3,7 @@ package com.calcit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
@@ -27,65 +28,106 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClick(View view)
     {
-        if (!currentScreen.equals("0")) {
-            switch (view.getId()) {
-                case R.id.button_0:
-                    currentScreen.concat("0");
-                    break;
-                case R.id.button_1:
-                    currentScreen.concat("1");
-                    break;
-                case R.id.button_2:
-                    currentScreen.concat("2");
-                    break;
-                case R.id.button_3:
-                    currentScreen.concat("3");
-                    break;
-                case R.id.button_4:
-                    currentScreen.concat("4");
-                    break;
-                case R.id.button_5:
-                    currentScreen.concat("5");
-                    break;
-                case R.id.button_6:
-                    currentScreen.concat("6");
-                    break;
-                case R.id.button_7:
-                    currentScreen.concat("7");
-                    break;
-                case R.id.button_8:
-                    currentScreen.concat("8");
-                    break;
-                case R.id.button_9:
-                    currentScreen.concat("9");
-                    break;
-            }
-            updateScreen();
+        //receive input
+        char in = 'n';
+        switch (view.getId()) {
+            case R.id.button_0:
+                in = '0';
+                break;
+            case R.id.button_1:
+                in = '1';
+                break;
+            case R.id.button_2:
+                in = '2';
+                break;
+            case R.id.button_3:
+                in = '3';
+                break;
+            case R.id.button_4:
+                in = '4';
+                break;
+            case R.id.button_5:
+                in = '5';
+                break;
+            case R.id.button_6:
+                in = '6';
+                break;
+            case R.id.button_7:
+                in = '7';
+                break;
+            case R.id.button_8:
+                in = '8';
+                break;
+            case R.id.button_9:
+                in = '9';
+                break;
+            case R.id.buttonPlus:
+                in = '+';
+                break;
+            case R.id.buttonMinus:
+                in = '-';
+                break;
+            case R.id.buttonMultiply:
+                in = '*';
+                break;
+            case R.id.buttonDivision:
+                in = '/';
+                break;
+            case R.id.buttonSin:
+                in = 's';
+                break;
+            case R.id.buttonCos:
+                in = 'c';
+                break;
+            case R.id.buttonTan:
+                in = 't';
+                break;
+            case R.id.buttonCalc:
+                in = '=';
+                break;
 
-            switch (view.getId()) {
-                case R.id.buttonPlus:
-                    operator = '+';
-                    break;
-                case R.id.buttonMinus:
-                    operator = '-';
-                    break;
-                case R.id.buttonMultiply:
-                    operator = '*';
-                    break;
-                case R.id.buttonDivision:
-                    operator = '/';
-                    break;
-                case R.id.buttonSin:
-                    operator = 's';
-                    break;
-                case R.id.buttonCos:
-                    operator = 'c';
-                    break;
-                case R.id.buttonTan:
-                    operator = 't';
-                    break;
+        }
+
+        //if this is a digit, it is probably a number the user is typing in
+          // check if the current value on screen is 0 or not,
+          //if 0
+        if(Character.isDigit(in))
+        {
+            if (currentScreen.equals("0"))
+            {
+                currentScreen = Character.toString(in);
+
+            }
+            else
+            {
+                currentScreen += Character.toString(in);
+            }
+
+            Log.d("VALUE",   "currentScreen is now " + currentScreen);
+            updateScreen();
+        }
+        else
+        {
+            //since the user input is strictly controlled through the buttons
+            //we can assume all non-digit input at this can only be operators
+            if (operand1 == 0)
+            {
+                operand1 = parseScreen();
+                operator = in;
+                operatorFlag = true;
+            }
+            else
+            {
+                if(operatorFlag && in == '=')
+                {
+                    operand2 = parseScreen();
+
+                    operatorFlag
+                }
             }
         }
+
+
     }
 
 
