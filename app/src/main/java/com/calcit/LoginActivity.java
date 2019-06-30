95d04d05fa139bb4ef1e5a78d305e921a2472f86
TinHuynh001 <Tin.Huynh001@umb.edu>
2019-06-30 17:26:48 -0500

    Facebook and Google signin integrated
    + Sign In completed on both platform, move to Main activity upon success. Can attempt the gradle requirement now.
    
    Notes:
    + The facebook/JSN errors are part of the FB SDK 5.1.0 build, can ignore
    + TODO: Separate under lying math code to another project to do a gradle import
    + TODO: Pass a user info from the success logins to MainActivity to ack login.
    + TODO: Make a logout button on Main Activity, return to LoginActivity. Also check to ensure only one account of any type is logged in at any time.
    + Code is a mess due to different approaches, clean when possible

--- app/src/main/java/com/calcit/LoginActivity.java
+++ app/src/main/java/com/calcit/LoginActivity.java
@@ -6,11 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookButtonBase;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,20 +28,49 @@ import com.google.android.gms.common.SignInButton;
 import android.view.View;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    GoogleSignInOptions gso;
-    GoogleSignInClient clientG;
+    private String user_firstname;
+    //decls for Google parts
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient clientG;
     private static final int RESULTCODE_SIGN_IN_GOOGLE = 9001;
+    private SignInButton btnGG;
 
-    SignInButton btnGG;
-    FacebookButtonBase btnFB;
+
+    //decls for Facebook part
+    private LoginButton btnFB;
+    private CallbackManager callbackManager;
+    //private static final int RESULTCODE_SIGN_IN_FACEBOOK= 9002;
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                   AccessToken currentAccessToken)
+        {
+            if(currentAccessToken == null)
+            {
+                Toast.makeText(LoginActivity.this, "User signed out.", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                loadUserProfile(currentAccessToken);
+            }
+
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Google inits
         //The Google sign-in button to authenticate the user. Note that this class only handles the
         //visual aspects of the button. In order to trigger an action, register a listener using
         // setOnClickListener(OnClickListener).
@@ -52,9 +88,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         clientG = GoogleSignIn.getClient(this, gso);
 
-        btnFB = findViewById(R.id.sign_in_button_Facebook);
-        btnFB.setOnClickListener(this);
 
+
+        //Facebook Inits
+        btnFB = findViewById(R.id.sign_in_button_Facebook);
+        callbackManager = CallbackManager.Factory.create();
+
+        btnFB.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel()
+            {
+                Toast.makeText(LoginActivity.this, "Sign in cancelled.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+
+            }
+        });
 
 
 
@@ -69,10 +128,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         clientG = GoogleSignIn.getClient(this, gso);
 
         // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        // the GoogleSignInAccount will be non-null.
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //goToMain(account);
 
-        //updateUI(account);
+        //Also check for logged-in Facebook accounts via the accessToken
+        //
+        //AccessToken tkn = AccessToken.getCurrentAccessToken();
+        //goToMain(tkn);
     }
 
     @Override
@@ -81,24 +144,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.sign_in_button_Google:
                 signInGoogle();
                 break;
-            case R.id.sign_in_button_Facebook:
-                signInFacebook();
-                break;
+
         }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        callbackManager.onActivityResult(requestCode,resultCode,data);
         super.onActivityResult(requestCode, resultCode, data);
         
         switch (requestCode)
         {
             case RESULTCODE_SIGN_IN_GOOGLE:
+                //super.onActivityResult(requestCode, resultCode, data);
                 // The Task returned from this call is always completed, no need to attach
                 // a listener.
                 Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
                 handleSignInResult(task);
                 break;
+                default:
         }
     }
 
@@ -107,12 +172,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            updateUI(account);
+            goToMain(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
 
-            updateUI(null);
+            goToMain((GoogleSignInAccount) null);
         }
     }
 
@@ -124,14 +189,52 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         startActivityForResult(signInIntent, RESULTCODE_SIGN_IN_GOOGLE);
     }
 
-    private void signInFacebook()
-    {
 
+    private void loadUserProfile(AccessToken access)
+    {
+        GraphRequest req = GraphRequest.newMeRequest(access, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response)
+            {
+                try
+                {
+                    user_firstname = object.getString("first_name");
+
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle params = new Bundle();
+        params.putString("fields", "first_name, last_name");
+        req.setParameters(params);
+        req.executeAsync();
     }
 
-    private void updateUI(@Nullable GoogleSignInAccount account)
+
+    //
+    private void goToMain(@Nullable GoogleSignInAccount account)
     {
         if (account != null)
+        {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        }
+        else
+        {
+            Toast.makeText(this, "Sign In failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void goToMain(AccessToken token)
+    {
+        if (token != null && !token.isExpired())
         {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
